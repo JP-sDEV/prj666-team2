16cbb6258b85959564ce916c69419f9097a53245
JP-sDEV <jon.pablo80@gmail.com>
2025-01-22 09:53:05 -0500

    refactor: revert to legacy setup

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// Note: using legacy format, refer to: https://github.com/eslint/eslint/issues/19322#issuecomment-2585395558
+
+module.exports = {
+  extends: ["next/core-web-vitals", "next", "eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  //   ESLint rules, refer: https://eslint.org/docs/latest/rules/
+  rules: {
+    "no-unused-vars": 2,
+    "no-const-assign": 2,
+    "no-dupe-else-if": 2,
+    "no-dupe-keys": 2,
+    "no-duplicate-imports": 1,
+    semi: 1,
+  },
+};
