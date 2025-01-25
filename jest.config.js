6125b7c7b4598349e402b4ae44233f84d4383d33
Mustafa Toygar Baykal <mtoygarby@gmail.com>
2025-01-25 15:26:19 -0500

    feature: implement backend unit testing with Jest

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,25 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config = {
   coverageProvider: 'v8',
-  testEnvironment: 'jsdom', //jsdom
+  testEnvironment: 'node', // Changed from 'jsdom' for API testing
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/*.test.js', '**/*.test.jsx'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
