372f000bb7ba1fc99c90d4b1166227d02ace94d2
sujungsong <songsujeong@songsujcbookpro.home>
2025-02-02 19:44:26 -0500

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Hero from "@/components/home-page/hero"
 import FeatureArticle from "@/components/home-page/feature-article"
+import Footer from "@/components/footer"
 import { Container, Main, Section } from "@/components/craft";
 
 export default function Page() {
@@ -9,6 +10,7 @@ export default function Page() {
         <Container>
           <Hero/>
           <FeatureArticle/>
+          <Footer/>
         </Container>
       </Section>
     </Main>
