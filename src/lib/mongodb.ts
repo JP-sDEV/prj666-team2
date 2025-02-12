808123552a9b12a945ff445f1bb0a1d4236627ae
Toygar Baykal <mtoygarby@gmail.com>
2025-02-12 15:13:21 -0500

    fix: handle undefined case

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,7 +7,7 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
@@ -21,7 +21,8 @@ if (process.env.NODE_ENV === 'development') {
     client = new MongoClient(uri, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = globalWithMongo._mongoClientPromise;
+  // We know _mongoClientPromise is defined at this point
+  clientPromise = globalWithMongo._mongoClientPromise!;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
