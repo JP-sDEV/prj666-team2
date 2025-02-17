f4f17f726968cd5b90f6500c8ebf1479a5879c17
Henly Su <henlysu.bz@gmail.com>
2025-02-17 17:26:26 -0500

    update

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -44,11 +44,7 @@ if (process.env.NODE_ENV === 'development') {
       return client;
     });
   }
-<<<<<<< HEAD
-  clientPromise = globalWithMongo._mongoClientPromise;
-=======
   clientPromise = globalWithMongo._mongoClientPromise!;
->>>>>>> feat/login
 } else {
   client = new MongoClient(uri, options);
   clientPromise = client.connect().then(async (client) => {
